7de968d2ef99ff38470a461b05858ed89c9b7a57
ecshenryho <ecshenryho@gmail.com>
2018-06-29 17:23:17 -0700

    initial code

---
+++ simulation.cpp
@@ -0,0 +1,43 @@
+#include<isotream>
+#include<fstream>
+using namespace std;
+
+class Simulation{
+private:
+    /* data */
+public:
+    Simulation(/* args */);
+    ~Simulation();
+};
+
+Simulation::Simulation(/* args */)
+{
+}
+
+Simulation::~Simulation()
+{
+}
+
+
+class Process{
+private:
+    /* data */
+public:
+    process(/* args */);~process();
+};
+
+process::process(/* args */)
+{
+}
+
+process::~process()
+{
+
+}
+
+int main(){
+
+
+    system("pause");
+    return 0;
+}
